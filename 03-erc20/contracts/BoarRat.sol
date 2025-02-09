9edb55d954329b7dfc269ea248a5df9b0f03bfc8
philippinecancer <eccepelagus>
2025-02-09 13:46:43 +0800

    wala lang edit edit lang

--- 03-erc20/contracts/BoarRat.sol
+++ 03-erc20/contracts/BoarRat.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NameYourToken is ERC20 {
+contract BoarRat is ERC20 {
     constructor(uint256 initialSupply) ERC20("NameYourToken", "NYT") {
         // _mint(msg.sender, initialSupply);
     }
